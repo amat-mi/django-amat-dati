ba8b2da508d63fcb8367d7a51a879296f193ae3e
caipirginka <github@oicom.com>
2015-11-17 16:40:22 +0100

    First experiments with Throttling and Paginating for "open" data.

--- open/urls.py
+++ open/urls.py
@@ -5,6 +5,7 @@ from rest_framework.routers import SimpleRouter
 
 from pinf.views import PinfSostaGiallobluViewSet, PinfSostaInvalidiViewSet, \
   PinfSostaMerciViewSet, PinfSostaTuristiciViewSet
+from open.views import OpenSostaMerciViewSet
 
 
 router = SimpleRouter()
@@ -12,7 +13,7 @@ router = SimpleRouter()
 ##### Elenco endpoints ################################
 router.register(r'sosta/gialloblu', PinfSostaGiallobluViewSet)
 router.register(r'sosta/invalidi', PinfSostaInvalidiViewSet)
-router.register(r'sosta/merci', PinfSostaMerciViewSet)
+router.register(r'sosta/merci', OpenSostaMerciViewSet)
 router.register(r'sosta/turistici', PinfSostaTuristiciViewSet)
 
 
--- open/views.py
+++ open/views.py
@@ -1,1 +1,36 @@
 # coding: utf-8
+from rest_framework import viewsets
+from rest_framework.decorators import list_route
+from rest_framework.throttling import UserRateThrottle
+
+from pinf.models import PinfSostaMerci
+from pinf.serializers import PinfSostaMerciSerializer
+
+#################################################
+class BurstRateThrottle(UserRateThrottle):
+    scope = 'burst'
+
+class SustainedRateThrottle(UserRateThrottle):
+    scope = 'sustained'
+
+#################################################
+class OpenSostaMerciViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = PinfSostaMerciSerializer
+    queryset = PinfSostaMerci.objects.all()
+    paginate_by = 10
+    
+    def list(self, request):
+        u"""
+        Returns all instances with Pagination but no Throttling.
+        """
+        return super(OpenSostaMerciViewSet, self).list(request)
+      
+    @list_route(
+                paginate_by = None,
+                throttle_classes = (BurstRateThrottle,SustainedRateThrottle,)
+                )
+    def all(self, request):
+        u"""
+        Returns all instances with Throttling but no Pagination.
+        """
+        return super(OpenSostaMerciViewSet, self).list(request)
