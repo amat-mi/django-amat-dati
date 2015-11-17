da35e4b015d8976d1aeaf9c25536658c8690b729
caipirginka <github@oicom.com>
2015-11-17 17:56:19 +0100

    More experiments with Throttling.

--- open/views.py
+++ open/views.py
@@ -1,10 +1,12 @@
 # coding: utf-8
 from rest_framework import viewsets
-from rest_framework.decorators import list_route
+from rest_framework.decorators import list_route, throttle_classes
 from rest_framework.throttling import UserRateThrottle
 
 from pinf.models import PinfSostaMerci
 from pinf.serializers import PinfSostaMerciSerializer
+from pinf.views import PinfSostaMerciViewSet
+
 
 #################################################
 class BurstRateThrottle(UserRateThrottle):
@@ -18,17 +20,31 @@ class OpenSostaMerciViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = PinfSostaMerciSerializer
     queryset = PinfSostaMerci.objects.all()
     paginate_by = 10
+#     throttle_classes = (BurstRateThrottle,SustainedRateThrottle,)
     
-    def list(self, request):
-        u"""
-        Returns all instances with Pagination but no Throttling.
+    def get_throttles(self):
         """
-        return super(OpenSostaMerciViewSet, self).list(request)
+        Instantiates and returns the list of throttles that this view uses based upon which action was called.
+        """
+        throttle_classes = (BurstRateThrottle,SustainedRateThrottle,) if self.action in ['list'] else () 
+        return [throttle() for throttle in throttle_classes]
+    
+# #     @throttle_classes((BurstRateThrottle,SustainedRateThrottle,))    
+#     def list(self, request, *args, **kwargs):
+#         u"""
+#         Returns all instances with Pagination but no Throttling.
+#         """
+#         return super(OpenSostaMerciViewSet, self).list(request, *args, **kwargs)
+#     list.throttle_classes = (BurstRateThrottle,SustainedRateThrottle,)
       
-    @list_route(
-                paginate_by = None,
-                throttle_classes = (BurstRateThrottle,SustainedRateThrottle,)
-                )
+#     @list_route(
+#                 paginate_by = None,
+#                 throttle_classes = (BurstRateThrottle,SustainedRateThrottle,)
+#                 )
+#     @list_route(
+#                 throttle_classes = ()
+#                 )
+    @list_route()
     def all(self, request):
         u"""
         Returns all instances with Throttling but no Pagination.
