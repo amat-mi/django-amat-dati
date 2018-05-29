a00649b4c3be25dcc117d494e881284935f9c014
caipirginka <github@oicom.com>
2018-05-29 15:31:15 +0200

    Aggiornata sintassi urls.py per Django 2.0.

--- server/urls.py
+++ server/urls.py
@@ -1,52 +1,53 @@
 # coding: utf-8
 
 from django.conf import settings
-from django.conf.urls import patterns, url, include
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.urls import re_path as url, include
 from django.views.generic.base import RedirectView
 from rest_framework.urlpatterns import format_suffix_patterns
 
 
-urlpatterns = patterns('',
+#from django.conf.urls import patterns, url, include
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 #     url(r'^docs/', include('rest_framework_swagger.urls')),
-)
+]
 
 # Default login/logout views
-urlpatterns += patterns('',
+urlpatterns += [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-)
+]
 
 # OAuth2 provider
-urlpatterns += patterns('',
+urlpatterns += [
     url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider'))
-)
+]
 
-urlpatterns += patterns('',
+urlpatterns += [
     url(r'^tweet/', include('tweet.urls', namespace='tweet')),
     # Questa è l'URL per la UI Client (in remoto gestita direttamente dal Web Server)
     url(r'^tweet/client/', RedirectView.as_view(url='/static/tweet/index.html', permanent=False), name="tweet-client"),    
-)
+]
 
-urlpatterns += patterns('',
+urlpatterns += [
     url(r'^park/', include('park_server_core.urls', namespace='park')),
-)
+]
 
-urlpatterns += patterns('',
+urlpatterns += [
     url(r'^pinf/', include('pinf.urls', namespace='pinf')),
-)
+]
 
-urlpatterns += patterns('',
+urlpatterns += [
     url(r'^open/', include('open.urls', namespace='open')),
-)
+]
 
 # Format suffixes
 urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'api'])
 
-urlpatterns += patterns('',
+urlpatterns += [
     url(r'^docs/', include('rest_framework_swagger.urls')),
-)
+]
 
 urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
